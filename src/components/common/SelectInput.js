ca547defb62bbffb1af1d8d5da705ea185919f2b
Raghav Bucha <55537545+RaghavB17@users.noreply.github.com>
2022-06-16 12:48:08 +0530

    implemented react testing

--- src/components/common/SelectInput.js
+++ src/components/common/SelectInput.js
@@ -8,13 +8,12 @@ const SelectInput = ({
   defaultOption,
   value,
   error,
-  options
+  options,
 }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <div className="field">
-        {/* Note, value is set here rather than on the option - docs: https://facebook.github.io/react/docs/forms.html */}
         <select
           name={name}
           value={value}
@@ -22,7 +21,7 @@ const SelectInput = ({
           className="form-control"
         >
           <option value="">{defaultOption}</option>
-          {options.map(option => {
+          {options.map((option) => {
             return (
               <option key={option.value} value={option.value}>
                 {option.text}
@@ -43,7 +42,7 @@ SelectInput.propTypes = {
   defaultOption: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   error: PropTypes.string,
-  options: PropTypes.arrayOf(PropTypes.object)
+  options: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default SelectInput;
